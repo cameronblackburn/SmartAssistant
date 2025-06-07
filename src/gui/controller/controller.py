2287630dc1dba6d8ef5e8443aa446ef06df72620
Cameron Blackburn <dev.cameronblackburn@outlook.com>
2025-06-07 01:28:00 +0100

    MVC structure gui

---
+++ src/gui/controller/controller.py
@@ -0,0 +1,12 @@
+# ------------------------------- Libraries ---------------------------
+import main_window
+from src.cli import parser
+from src.tts import offline_tts
+# ------------------------------- Constants ---------------------------
+
+# ------------------------------- Methods -----------------------------
+
+def update_window():
+    """Method for outputting and clearing gui"""
+    return
+
--- src/gui/view/main_window.py
+++ src/gui/view/main_window.py
@@ -1,6 +1,5 @@
 # ------------------------------- Libraries ---------------------------
 import sys
-import random
 from PySide6 import QtCore, QtWidgets, QtGui
 
 # ------------------------------- Constants ---------------------------
@@ -9,7 +8,7 @@ from PySide6 import QtCore, QtWidgets, QtGui
 
 # ----------------------------- GUI Setup -----------------------------
 
-class MyWidget(QtWidgets.QWidget):
+class MyMainWindow(QtWidgets.QWidget):
     """Main window"""
     def __init__(self):
         super().__init__()
@@ -18,16 +17,24 @@ class MyWidget(QtWidgets.QWidget):
         self.smartAssOut.setReadOnly(True)
         self.smartAssOut.append("Testing")
 
-        self.textbox = QtWidgets.QLineEdit(self) # Textbox for user input
+        self.textbox = self.create_textbox() # Textbox for user input
 
         self.layout = QtWidgets.QVBoxLayout(self) # Layout manager
         self.layout.addWidget(self.smartAssOut)
         self.layout.addWidget(self.textbox)
 
+    def create_textbox(self):
+
+        textbox = QtWidgets.QLineEdit()
+        textbox.setPlaceholderText("Type your command here...")
+        #---> Set action for textbox 
+        return textbox
+
+
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
 
-    widget = MyWidget()
+    widget = MyMainWindow()
     widget.resize(800, 600)
     widget.show()
 
